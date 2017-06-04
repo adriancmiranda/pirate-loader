d6eb3c65274d310bcd58b6d3ab1b1595897e8113
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-06-04 18:03:21 -0300

    test: structure

--- test/specs/index.spec.js
+++ test/specs/index.spec.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import test from 'ava-spec';
-import webpack from './webpack';
 import Text from 'extract-text-webpack-plugin';
+import webpack from '../webpack';
 
 test('pirate-loader', t => {
 	t.is(toString.call(webpack), '[object Function]');
